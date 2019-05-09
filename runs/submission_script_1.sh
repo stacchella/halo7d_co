2e4628d822b8017c556452de78ce952590f581b8
stacchella <sandro.tacchella@cfa.harvard.edu>
2019-05-09 16:24:04 -0400

    live points increased

--- runs/submission_script_1.sh
+++ runs/submission_script_1.sh
@@ -31,7 +31,7 @@ srun -n 1 python /n/conroyfs1/stacchella/halo7d_co/runs/param_file_parametric_el
 --add_agn \
 --dynesty \
 --nested_method="rwalk" \
---nlive_batch=200 \
---nlive_init=200 \
+--nlive_batch=1000 \
+--nlive_init=1000 \
 --nested_dlogz_init=0.1 \
 --nested_maxcall=10000 
