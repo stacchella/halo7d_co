2d23e2d2ed8eaca499740f3eb5e4e327efb89136
stacchella <sandro.tacchella@cfa.harvard.edu>
2019-05-08 23:41:10 -0400

    push

--- runs/submission_script_1.sh
+++ runs/submission_script_1.sh
@@ -10,24 +10,28 @@
 #SBATCH -p conroy,itc_cluster,hernquist,shared
 ### memory per cpu, in MB
 #SBATCH --mem-per-cpu=4000
+### constraints
+#SBATCH --constraint=intel
 ### Job name
-#SBATCH -J 'halo7d_simple'
+#SBATCH -J 'halo7d_full'
 ### output and error logs
-#SBATCH -o simple_%a.out
-#SBATCH -e simple_%a.err
+#SBATCH -o full_%a.out
+#SBATCH -e full_%a.err
 ### mail
 #SBATCH --mail-type=END
 #SBATCH --mail-user=sandro.tacchella@cfa.harvard.edu
 source activate pro
 srun -n 1 python /n/conroyfs1/stacchella/halo7d_co/runs/param_file_parametric.py \
 --objid="${SLURM_ARRAY_TASK_ID}" \
---outfile="parametric_simple" \
---err_floor=0.1 \
---remove_mips24 \
+--outfile="parametric_withEL_full" \
+--err_floor=0.05 \
+--S2N_cut=5.0 \
 --fit_continuum \
+--add_duste \
+--add_agn \
 --dynesty \
 --nested_method="rwalk" \
 --nlive_batch=200 \
 --nlive_init=200 \
---nested_dlogz_init=0.5 \
+--nested_dlogz_init=0.1 \
 --nested_maxcall=10000 
