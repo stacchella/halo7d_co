cd29b44b9212adaeb1e25e9e652415d3463c688e
stacchella <sandro.tacchella@cfa.harvard.edu>
2019-06-16 21:23:49 -0400

    added new param file

---
+++ runs/submission_script_jitter.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+### Name of the job
+### Requested number of cores
+#SBATCH -n 1
+### Requested number of nodes
+#SBATCH -N 1
+### Requested computing time in minutes
+#SBATCH -t 10080
+### Partition or queue name
+#SBATCH -p conroy,itc_cluster,hernquist,shared
+### memory per cpu, in MB
+#SBATCH --mem-per-cpu=4000
+### constraints
+#SBATCH --constraint=intel
+### Job name
+#SBATCH -J 'halo7dwj'
+### output and error logs
+#SBATCH -o halo7dwj_%a.out
+#SBATCH -e halo7dwj_%a.err
+### mail
+#SBATCH --mail-type=END
+#SBATCH --mail-user=sandro.tacchella@cfa.harvard.edu
+source activate pro
+srun -n 1 python /n/conroyfs1/stacchella/halo7d_co/runs/param_file_parametric_eline_marg_fout_jitter.py \
+--objid="${SLURM_ARRAY_TASK_ID}" \
+--outfile="halo7d_parametric_withjitter" \
+--err_floor_phot=0.05 \
+--err_floor_spec=0.1 \
+--f_boost=10.0 \
+--S2N_cut=5.0 \
+--fit_continuum \
+--add_duste \
+--add_agn \
+--dynesty \
+--nested_method="rwalk" \
+--nlive_batch=120 \
+--nlive_init=120 \
+--nested_posterior_thresh=0.5 \
+--nested_dlogz_init=0.5 \
+--nested_maxcall=3000000 
