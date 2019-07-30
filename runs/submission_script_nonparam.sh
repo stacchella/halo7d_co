517bf8f1fcb05d192e54670126577dac5d33a8f7
stacchella <sandro.tacchella@cfa.harvard.edu>
2019-07-30 10:42:06 -0700

    prior update

--- runs/submission_script_nonparam.sh
+++ runs/submission_script_nonparam.sh
@@ -13,17 +13,17 @@
 ### constraints
 #SBATCH --constraint=intel
 ### Job name
-#SBATCH -J 'non2halo7d'
+#SBATCH -J 'nonhalo7d'
 ### output and error logs
-#SBATCH -o non2halo7d_%a.out
-#SBATCH -e non2halo7d_%a.err
+#SBATCH -o nonhalo7d_%a.out
+#SBATCH -e nonhalo7d_%a.err
 ### mail
 #SBATCH --mail-type=END
 #SBATCH --mail-user=sandro.tacchella@cfa.harvard.edu
 source activate pro
 srun -n 1 python /n/conroyfs1/stacchella/halo7d_co/runs/param_file.py \
 --objid="${SLURM_ARRAY_TASK_ID}" \
---outfile="halo7d_nonparametric_2" \
+--outfile="halo7d_nonparametric" \
 --non_param_sfh \
 --err_floor_phot=0.05 \
 --err_floor_spec=0.001 \
