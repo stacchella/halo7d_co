dd363a44681b0b3da8708b733683b784daa59a2a
stacchella <sandro.tacchella@cfa.harvard.edu>
2019-11-13 14:18:23 -0500

    updated post processing runs

---
+++ runs/submission_script_postpro_nonparam_8.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+### Name of the job
+### Requested number of cores
+#SBATCH -n 1
+### Requested number of nodes
+#SBATCH -N 1
+### Requested computing time in minutes
+#SBATCH -t 10080
+### Partition or queue name
+#SBATCH -p conroy,itc_cluster,hernquist,shared
+### memory per cpu, in MB
+#SBATCH --mem-per-cpu=4000
+### Job name
+#SBATCH -J 'pp_8'
+### output and error logs
+#SBATCH -o pp_8_%a.out
+#SBATCH -e pp_8_%a.err
+### mail
+#SBATCH --mail-type=END
+#SBATCH --mail-user=sandro.tacchella@cfa.harvard.edu
+module load python/2.7.14-fasrc01
+source activate pro
+srun -n 1 python /n/conroyfs1/stacchella/halo7d_co/scripts/draw_posterior_output_cluster.py \
+--number_of_bins=200 \
+--idx_file_key="${SLURM_ARRAY_TASK_ID}" \
+--path_results="/n/conroyfs1/stacchella/halo7d_co/results/nonparam_8/" \
+--ncalc=1000 \
+--non_param_sfh \
+--n_bins_sfh=8 \
+--add_jitter \
+--fit_continuum \
+--add_duste \
+--add_agn \
+--restrict_dust_agn \
+--restrict_prior
