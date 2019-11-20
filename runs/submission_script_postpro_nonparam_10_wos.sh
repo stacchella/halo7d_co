1c2d6bf22077b01d5b061e48218ccaede1b73afd
stacchella <sandro.tacchella@cfa.harvard.edu>
2019-11-20 09:35:30 +0100

    added postproc submission file

--- runs/submission_script_postpro_nonparam_10_wos.sh
+++ runs/submission_script_postpro_nonparam_10_wos.sh
@@ -11,23 +11,24 @@
 ### memory per cpu, in MB
 #SBATCH --mem-per-cpu=4000
 ### Job name
-#SBATCH -J 'poprnplimitedwos'
+#SBATCH -J 'pp_10wos'
 ### output and error logs
-#SBATCH -o poprnpwoslimited_%a.out
-#SBATCH -e poprnpwoslimited_%a.err
+#SBATCH -o pp_10wos_%a.out
+#SBATCH -e pp_10wos_%a.err
 ### mail
 #SBATCH --mail-type=END
 #SBATCH --mail-user=sandro.tacchella@cfa.harvard.edu
+module load python/2.7.14-fasrc01
 source activate pro
 srun -n 1 python /n/conroyfs1/stacchella/halo7d_co/scripts/draw_posterior_output_cluster.py \
 --number_of_bins=200 \
 --idx_file_key="${SLURM_ARRAY_TASK_ID}" \
---path_results="/n/conroyfs1/stacchella/halo7d_co/results/nonparam_limited_wos/" \
+--path_results="/n/conroyfs1/stacchella/halo7d_co/results/nonparam_10_wos/" \
 --ncalc=1000 \
 --non_param_sfh \
+--n_bins_sfh=10 \
 --switch_off_spec \
 --add_duste \
 --add_agn \
 --restrict_dust_agn \
---restrict_prior \
---remove_mips24
+--restrict_prior
