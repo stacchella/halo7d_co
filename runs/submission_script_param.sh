d418e9a682ed0f8ac3269fbb3de6d841072bf3ec
stacchella <sandro.tacchella@cfa.harvard.edu>
2020-02-26 15:10:52 -0500

    added new param file

--- runs/submission_script_param.sh
+++ runs/submission_script_param.sh
@@ -22,7 +22,7 @@
 #SBATCH --mail-user=sandro.tacchella@cfa.harvard.edu
 module load python/2.7.14-fasrc01
 source activate pro
-srun -n 1 python /n/conroyfs1/stacchella/halo7d_co/runs/param_file.py \
+srun -n 1 python /n/conroyfs1/stacchella/halo7d_co/runs/halo7d_param_file.py \
 --objid="${SLURM_ARRAY_TASK_ID}" \
 --outfile="halo7d_parametric" \
 --err_floor_phot=0.05 \
