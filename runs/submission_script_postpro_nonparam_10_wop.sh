4af3539ed1dcace935755b99e67694c65f0d33dc
stacchella <sandro.tacchella@cfa.harvard.edu>
2020-03-26 09:36:58 -0400

    new submission script

---
+++ runs/submission_script_postpro_nonparam_10_wop.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+### Name of the job
+### Requested number of cores
+#SBATCH -n 1
+### Requested number of nodes
+#SBATCH -N 1
+### Requested computing time in minutes
+#SBATCH -t 10080
+### Partition or queue name
+#SBATCH -p conroy,itc_cluster,hernquist,shared
+### memory per cpu, in MB
+#SBATCH --mem-per-cpu=4000
+### Job name
+#SBATCH -J 'pp_10wop'
+### output and error logs
+#SBATCH -o pp_10wop_%a.out
+#SBATCH -e pp_10wop_%a.err
+### mail
+#SBATCH --mail-type=END
+#SBATCH --mail-user=sandro.tacchella@cfa.harvard.edu
+module load python/2.7.14-fasrc01
+source activate pro
+srun -n 1 python /n/conroyfs1/stacchella/halo7d_co/scripts/draw_posterior_output_cluster.py \
+--number_of_bins=200 \
+--idx_file_key="${SLURM_ARRAY_TASK_ID}" \
+--path_results="/n/conroyfs1/stacchella/halo7d_co/results/nonparam_10_wop/" \
+--ncalc=1000 \
+--non_param_sfh \
+--n_bins_sfh=10 \
+--add_jitter \
+--add_neb \
+--fit_continuum \
+--add_duste \
+--add_agn \
